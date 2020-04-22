0a2b31b24b367fdb7c34731b0b68bdc607d8f389
masoudNemati <masoud.nnr@gmail.com>
2020-04-22 13:23:32 +0430

    installed axios and used async await

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
 import Navbar from "./components/layout/Navbar.js";
 import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users?since=135");
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div>
@@ -11,7 +21,7 @@ class App extends Component {
           <Navbar />
         </div>
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,41 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        login: "naghi",
-        id: 136,
-        node_id: "MDQ6VXNlcjEzNg==",
-        avatar_url: "https://avatars2.githubusercontent.com/u/136?v=4",
-        gravatar_id: "",
-        url: "https://api.github.com/users/simonjefford",
-        html_url: "https://github.com/simonjefford",
-      },
-      {
-        login: "josh",
-        id: 137,
-        node_id: "MDQ6VXNlcjEzNw==",
-        avatar_url: "https://avatars2.githubusercontent.com/u/137?v=4",
-        gravatar_id: "",
-        url: "https://api.github.com/users/josh",
-        html_url: "https://github.com/josh",
-      },
-      {
-        login: "stevedekorte",
-        id: 138,
-        node_id: "MDQ6VXNlcjEzOA==",
-        avatar_url: "https://avatars3.githubusercontent.com/u/138?v=4",
-        gravatar_id: "",
-        url: "https://api.github.com/users/stevedekorte",
-        html_url: "https://github.com/stevedekorte",
-      },
-    ],
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
