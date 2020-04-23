b5c26ad8bea9d9cc6ad13efd794c2e3cece2236d
masoudNemati <masoud.nnr@gmail.com>
2020-04-23 16:16:02 +0430

    added search box that actually works and search github for users

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
 import Navbar from "./components/layout/Navbar.js";
+import Search from "./components/users/Search";
 import Users from "./components/users/Users";
 
 class App extends Component {
@@ -9,14 +10,17 @@ class App extends Component {
     users: [],
     loading: false,
   };
-  async componentDidMount() {
+
+  //search github users
+  searchUsers = async (text) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
       client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div>
@@ -24,6 +28,7 @@ class App extends Component {
           <Navbar />
         </div>
         <div className="container">
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
