dba234e85260f0aeaed97d5a303de8abb986aaa3
masoudNemati <masoud.nnr@gmail.com>
2020-04-26 02:33:51 +0430

    created user UI card including name, bio, followers,following and etc the font awesome bug started from here

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
   componentDidMount() {
@@ -10,6 +12,7 @@ export class User extends Component {
       avatar_url,
       location,
       bio,
+      company,
       blog,
       login,
       html_url,
@@ -21,7 +24,54 @@ export class User extends Component {
     } = this.props.user;
 
     const { loading } = this.props;
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-light">
+          Back to search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fa fa-unlock" style={{ color: "green" }} />
+        ) : (
+          <i className="fa fa-times-circle " style={{ color: "red" }} />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              alt="Avatar"
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>Location:{location} </p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio:</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visit Github profile
+            </a>
+            <ul>
+              <li>{login && <Fragment>username: {login}</Fragment>}</li>
+              <li>{company && <Fragment>Company: {company}</Fragment>}</li>
+              <li>{blog && <Fragment>Website: {blog}</Fragment>}</li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-primary">Followers:{followers} </div>
+          <div className="badge badge-success">Following:{following} </div>
+          <div className="badge badge-light">Public Repos:{public_repos} </div>
+          <div className="badge badge-dark">Public Gists:{public_gists} </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
