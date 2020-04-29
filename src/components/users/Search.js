04d178b34d1ea110bcbb20767d452336ce340e03
masoudNemati <masoud.nnr@gmail.com>
2020-04-29 15:39:58 +0430

    changed class based components to functional components by using hooks

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,45 +1,41 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export class Search extends Component {
-  state = {
-    text: "",
-  };
-  onSubmit = (e) => {
+const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
+  const [text, setText] = useState("");
+
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.setAlert("please enter something", "light");
+    if (text === "") {
+      setAlert("please enter something", "light");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      searchUsers(text);
+      setText("");
     }
   };
-  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
-  render() {
-    const { showClear, clearUsers } = this.props;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input
-            type="text"
-            name="text"
-            placeholder="Search for github users..."
-            value={this.state.text}
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {showClear && (
-          <button className="btn btn-light btn-block" onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+  const onChange = (e) => setText(e.target.value);
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="text"
+          placeholder="Search for github users..."
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Search;
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,81 +1,80 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
 import { Link } from "react-router-dom";
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      company,
-      blog,
-      login,
-      html_url,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-    } = this.props.user;
+const User = ({ loading, repos, getUser, getUserRepos, match, user }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    //eslint-disable-next-line
+  }, []);
 
-    const { loading, repos } = this.props;
-    if (loading) return <Spinner />;
-    return (
-      <Fragment>
-        <Link to="/" className="btn btn-light">
-          Back to search
-        </Link>
-        Hireable:{" "}
-        {hireable ? (
-          <i className="fa fa-unlock" style={{ color: "green" }} />
-        ) : (
-          <i className="fa fa-times-circle " style={{ color: "red" }} />
-        )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              className="round-img"
-              alt="Avatar"
-              style={{ width: "150px" }}
-            />
-            <h1>{name}</h1>
-            <p>Location:{location} </p>
-          </div>
-          <div>
-            {bio && (
-              <Fragment>
-                <h3>Bio:</h3>
-                <p>{bio}</p>
-              </Fragment>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              Visit Github profile
-            </a>
-            <ul>
-              <li>{login && <Fragment>username: {login}</Fragment>}</li>
-              <li>{company && <Fragment>Company: {company}</Fragment>}</li>
-              <li>{blog && <Fragment>Website: {blog}</Fragment>}</li>
-            </ul>
-          </div>
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    company,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  if (loading) return <Spinner />;
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-light">
+        Back to search
+      </Link>
+      Hireable:{" "}
+      {hireable ? (
+        <i className="fa fa-unlock" style={{ color: "green" }} />
+      ) : (
+        <i className="fa fa-times-circle " style={{ color: "red" }} />
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            className="round-img"
+            alt="Avatar"
+            style={{ width: "150px" }}
+          />
+          <h1>{name}</h1>
+          <p>Location:{location} </p>
         </div>
-        <div className="card text-center">
-          <div className="badge badge-primary">Followers:{followers} </div>
-          <div className="badge badge-success">Following:{following} </div>
-          <div className="badge badge-light">Public Repos:{public_repos} </div>
-          <div className="badge badge-dark">Public Gists:{public_gists} </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio:</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit Github profile
+          </a>
+          <ul>
+            <li>{login && <Fragment>username: {login}</Fragment>}</li>
+            <li>{company && <Fragment>Company: {company}</Fragment>}</li>
+            <li>{blog && <Fragment>Website: {blog}</Fragment>}</li>
+          </ul>
         </div>
-        <Repos repos={repos} />
-      </Fragment>
-    );
-  }
-}
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers:{followers} </div>
+        <div className="badge badge-success">Following:{following} </div>
+        <div className="badge badge-light">Public Repos:{public_repos} </div>
+        <div className="badge badge-dark">Public Gists:{public_gists} </div>
+      </div>
+      <Repos repos={repos} />
+    </Fragment>
+  );
+};
 
 export default User;
